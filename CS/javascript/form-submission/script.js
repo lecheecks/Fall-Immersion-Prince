74bc9ae21a059fc6b3734fede82fca0601e4653c
lecheecks <prince.montgomery@hgs.hiddengeniusproject.org>
2024-10-12 12:41:55 -0700

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,16 @@
-function formSubmission() {
-    // Your code here.
-}
+ocument.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('myForm');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        //Add Varibles from index.html (hint: Use document.get function)
+
+        if (name && email && message) {
+            alert(`Form submitted successfully!\nName: ${name}\nEmail: ${email}\nMessage: ${message}`);
+        } else {
+            alert('Please fill all the fields');
+        }
+    });
+});
+//Add additional fields
